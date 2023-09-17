3e07c8724f547d6f160a679772b3399159f95f51
psbautomationuser1 <145265878+psbautomationuser1@users.noreply.github.com>
2023-09-17 19:02:04 +0530

    Update StaticEx.java
    My First Commit

--- StaticEx.java
+++ StaticEx.java
@@ -4,7 +4,7 @@ public class StaticEx {
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		//This is updated by user01
 		
 		System.out.println("I am From Main");
 	}
